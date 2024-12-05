257f415dd3a5e3ee7c23c51414ecf6ce268d278a
Matias P. <matias.penailillob@gmail.com>
2024-12-05 18:49:02 -0300

    refactor: orders controller

--- src/orders/orders.controller.ts
+++ src/orders/orders.controller.ts
@@ -1,7 +1,7 @@
 import { Controller, Get, Post, Body, Patch, Param, Delete, Inject, ParseUUIDPipe, Query } from '@nestjs/common';
 import { CreateOrderDto } from './dto/create-order.dto';
 import { ClientProxy, RpcException } from '@nestjs/microservices';
-import { ORDERS_SERVICE } from 'src/config/services';
+import { NATS_SERVICE } from 'src/config/services';
 import { firstValueFrom } from 'rxjs';
 import { OrderPaginationDto } from './dto/order-pagination.dto';
 import { PaginationDto } from 'src/common/dto/pagination.dto';
@@ -9,22 +9,22 @@ import { StatusDto } from './dto/status.dto';
 
 @Controller('orders')
 export class OrdersController {
-  constructor( @Inject(ORDERS_SERVICE) private readonly ordersClient: ClientProxy) {}
+  constructor( @Inject(NATS_SERVICE) private readonly client: ClientProxy) {}
 
   @Post()
   create(@Body() createOrderDto: CreateOrderDto) {
-    return this.ordersClient.send('createOrder', createOrderDto);
+    return this.client.send('createOrder', createOrderDto);
   }
 
   @Get()
   findAll(@Query() orderPaginationDto: OrderPaginationDto) {
-    return this.ordersClient.send('findAllOrders', orderPaginationDto)
+    return this.client.send('findAllOrders', orderPaginationDto)
   }
 
   @Get('id/:id')
   async findOne(@Param('id', ParseUUIDPipe) id: string) {    
     try {
-      const order = await firstValueFrom(this.ordersClient.send('findOneOrder', { id }))
+      const order = await firstValueFrom(this.client.send('findOneOrder', { id }))
       return order;
     } catch (error) {
       throw new RpcException(error)
@@ -34,7 +34,7 @@ export class OrdersController {
   @Get(':status')
   async findAllByStatus(@Param() statusDto: StatusDto, @Query() paginationDto: PaginationDto) {    
     try {
-      return this.ordersClient.send('findAllOrders', {
+      return this.client.send('findAllOrders', {
         ...paginationDto,
         status: statusDto.status
       });
@@ -46,7 +46,7 @@ export class OrdersController {
   @Patch(':id')
   async changeOrderStatus(@Param('id', ParseUUIDPipe) id: string, @Body() statusDto: StatusDto) {
     try {
-      return this.ordersClient.send('changeOrderStatus', { id, status: statusDto.status})
+      return this.client.send('changeOrderStatus', { id, status: statusDto.status})
     } catch (error) {
       throw new RpcException(error)
     }
